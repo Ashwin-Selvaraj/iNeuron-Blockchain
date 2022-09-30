42e9009cb9c3de2758f7ac1f76a224419fa959cb
Ashwin-Selvaraj <85512351+Ashwin-Selvaraj@users.noreply.github.com>
2022-09-30 15:39:58 +0530

    Add files via upload

---
+++ Javascript/Assignment_2/studentGrade.js
@@ -0,0 +1,38 @@
+function studentGrade(marks) {
+    switch (true) {
+        case (marks>=90 && marks <=100):
+            console.log("S grade");
+            break;
+        
+        case (marks>=80 && marks <90):
+            console.log("A grade");
+            break;
+
+        case (marks>=70 && marks <80):
+            console.log("B grade");
+            break;
+
+        case (marks>=60 && marks <70):
+            console.log("C grade");
+            break;
+
+        case (marks>=50 && marks <60):
+            console.log("D grade");
+            break;
+
+        case (marks>=40 && marks <50):
+            console.log("E grade");
+            break;
+        
+        case marks<40:
+            console.log("Fail");
+            break;
+
+        default:
+            console.log("Invalid marks");
+    }
+
+
+}
+
+studentGrade(99);
