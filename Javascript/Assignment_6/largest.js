8d41048adc19196bff3fd844a2a7178ce08f50a1
Ashwin-Selvaraj <85512351+Ashwin-Selvaraj@users.noreply.github.com>
2022-10-02 12:06:37 +0530

    Add files via upload

---
+++ Javascript/Assignment_6/largest.js
@@ -0,0 +1,31 @@
+function find_largest(n) {
+  let arr = [3, 45, 6, 7, 23, 5, 7, 8];
+  let arr1 = [];
+  let dummy = [3, 45, 6, 7, 23, 5, 7, 8];
+  for (let j = 0; j < arr.length; j++) {
+    let max = 0;
+    for (let i = 0; i < dummy.length; i++) {
+      if (dummy[i] > max) {
+        max = dummy[i];
+      } else {
+        max = max;
+      }
+    }
+
+    arr1.push(max);
+    let a = dummy.indexOf(max);
+    if (dummy.length > 1) {
+      dummy.splice(a, 1);
+    }
+    // console.log(arr1);
+  }
+
+  console.log(arr1[n - 1]);
+}
+
+find_largest(1);
+
+
+
+
+
