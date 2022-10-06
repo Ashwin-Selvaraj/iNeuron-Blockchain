2e7bebdb7ffe46b48aa82ac1d0123f20c0271f70
Ashwin-Selvaraj <85512351+Ashwin-Selvaraj@users.noreply.github.com>
2022-10-06 22:18:38 +0530

    Update Q1_truncate.js

--- Javascript/Assignment_7/Q1_truncate.js
+++ Javascript/Assignment_7/Q1_truncate.js
@@ -1,1 +1,20 @@
+let S="icecream"
 
+if(S.length>4)
+{
+    let i=4;
+    let finalStr=""
+    let itr = S[Symbol.iterator]()
+    let res = itr.next();
+    while(i>0)
+    {
+        finalStr=finalStr + res.value;
+        res=itr.next();
+        i--;
+    }
+    console.log(finalStr);
+}
+else
+{
+    console.log(S);
+}
