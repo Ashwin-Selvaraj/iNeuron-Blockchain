20fc0cc5d22f3c40eaac007475fa3f1e6e98d625
Ashwin-Selvaraj <85512351+Ashwin-Selvaraj@users.noreply.github.com>
2022-10-18 19:37:48 +0530

    Create Q3_set.js

---
+++ Javascript/Assignment_8/Q3_set.js
@@ -0,0 +1,10 @@
+let set1 = new Set();
+while (set1.size < 4) {
+  set1.add(Math.round(Math.random() * 10));
+}
+console.log(set1);
+if (set1.has(8)) {
+  console.log("The created set has 8 in it");
+} else {
+  console.log("The created set doesn't have 8 in it");
+}
