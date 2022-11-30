4444dc319512e48bc855ee3751c638aae3a7395d
Ashwin-Selvaraj <85512351+Ashwin-Selvaraj@users.noreply.github.com>
2022-11-30 09:44:38 +0530

    Update leap_year.js

--- Javascript/Assignment_1/leap_year.js
+++ Javascript/Assignment_1/leap_year.js
@@ -7,10 +7,3 @@ else if(year%4 == 0 && year%100 !=0 && year%400 != 0)
 {
     console.log(year+" is a leap year");
 }
-else if(year%4 == 0 && year%100 ==0 && year%400 != 0)
-{
-    console.log(year+" is not a Leap Year");
-}
-else{
-    console.log(year+" is not a Leap Year");
-}
